c1cefa54a42c4105b6ffc008ee67a3bc094c0f38
Rustam Shrestha <santosh.787402@smc.tu.edu.np>
2026-01-30 08:40:42 +0545

    added hero elements and enhanced navbar for further change for UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import Header from './components/Header'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
 
 function App() {
   return (
-    <Layout>
-      <Header />
-    </Layout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/search" element={<>search page</>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,15 +1,11 @@
-const Layout  = ({ children }: { children: React.ReactNode }) => {
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+const Layout  = () => {
   return (
     <div className="min-h-screen flex flex-col">
-        <header className="bg-blue-600 text-white p-4">
-            <h1 className="text-2xl font-bold">Booking Application</h1>
-        </header>
-        <main className="flex-grow container mx-auto p-4">
-            {children}
-        </main>
-        <footer className="bg-gray-200 text-center p-4">
-            &copy; {new Date().getFullYear()} Booking Application. All rights reserved
-        </footer>
+       <Header />
+       <Hero />
     </div>
   );
 }
